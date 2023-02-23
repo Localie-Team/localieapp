e6decddd149351004256fcb541110dd002cd7fb3
Khoa Ly <kly27@calpoly.edu>
2023-02-22 23:20:30 -0800

    feat: navigation handler code

--- app/src/main/java/com/example/localieapp/DashboardActivity.kt
+++ app/src/main/java/com/example/localieapp/DashboardActivity.kt
@@ -1,4 +1,74 @@
 package com.example.localieapp
 
-class DashboardActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import com.example.localieapp.ProfileFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class DashboardActivity : AppCompatActivity() {
+    private var firebaseAuth: FirebaseAuth? = null
+    var firebaseUser: FirebaseUser? = null
+    var myuid: String? = null
+    var navigationView: TabLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        navigationView = findViewById(R.id.dashboard_tab_layout)
+        navigationView!!.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                    when (tab!!.id) {
+
+                        R.id.nav_profile ->
+                        {
+                            ProfileFragment fragment = new ProfileFragment();
+                            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            fragmentTransaction.replace(R.id.content, fragment, "");
+                            fragmentTransaction.commit();
+                        }
+
+                        R.id.nav_deals ->
+                        {
+                            DealsFragment fragment = new DealsFragment();
+                            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            fragmentTransaction.replace(R.id.content, fragment, "");
+                            fragmentTransaction.commit();
+                        }
+
+                        R.id.nav_earn ->
+                        {
+                            EarnFragment fragment = new EarnFragment();
+                            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                            fragmentTransaction.replace(R.id.content, fragment, "");
+                            fragmentTransaction.commit();
+                        }
+                    }
+
+                    // Handle tab select
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Handle tab reselect
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Handle tab unselect
+            }
+        })
+
+        // When we open the application first
+        // time the fragment should be shown to the user
+        // in this case it is home fragment
+        val fragment = DealsFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.content, fragment, "")
+        fragmentTransaction.commit()
+    }
+
 }
