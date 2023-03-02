18fefff66c6d716c7b99ad0d570fc5cfe074df39
Giselle Dougan <giselle.dougan@hotmail.com>
2023-03-01 22:11:03 -0800

    feat: firebase storage init

--- app/src/main/java/com/example/localieapp/DashboardActivity.kt
+++ app/src/main/java/com/example/localieapp/DashboardActivity.kt
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 class DashboardActivity : AppCompatActivity() {
     private var firebaseAuth: FirebaseAuth? = null
+    private var storage: FirebaseStorage? = null
     var firebaseUser: FirebaseUser? = null
     var myuid: String? = null
     var navigationView: TabLayout? = null
@@ -18,29 +22,33 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        // Firebase Storage init
+        storage = Firebase.storage
+
         firebaseAuth = FirebaseAuth.getInstance()
+
         navigationView = findViewById(R.id.dashboard_tab_layout)
-        val tab = navigationView!!.getTabAt(1);
-        tab?.select();
+        val tab = navigationView!!.getTabAt(1)
+        tab?.select()
 
         navigationView!!.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                    var fragment: Fragment? = null;
+                    var fragment: Fragment? = null
 
                     when (tab!!.position) {
 
-                        0 -> fragment = ProfileFragment();
-                        1 -> fragment = DealsFragment();
-                        2-> fragment = EarnFragment();
+                        0 -> fragment = ProfileFragment()
+                        1 -> fragment = DealsFragment()
+                        2-> fragment = EarnFragment()
                     }
 
 
                     if(fragment != null) {
-                        Log.d("TAG", fragment.toString());
-                        val fragmentTransaction = supportFragmentManager.beginTransaction();
-                        fragmentTransaction.replace(R.id.content, fragment, "");
-                        fragmentTransaction.commit();
+                        Log.d("TAG", fragment.toString())
+                        val fragmentTransaction = supportFragmentManager.beginTransaction()
+                        fragmentTransaction.replace(R.id.content, fragment, "")
+                        fragmentTransaction.commit()
                     }
 
                     // Handle tab select
