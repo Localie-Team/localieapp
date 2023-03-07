e16a8ebba2be87c89e0aa2bd87f485ea26790848
Khoa Ly <kly27@calpoly.edu>
2023-03-07 13:24:17 -0800

    dep: added storage sdk

--- app/src/main/java/com/example/localieapp/data/Datasource.kt
+++ app/src/main/java/com/example/localieapp/data/Datasource.kt
@@ -2,9 +2,13 @@ package com.example.localieapp.data
 import com.example.localieapp.R
 import com.example.localieapp.adapter.ExpandableListAdapter
 import com.example.localieapp.model.Coupon
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Datasource {
     fun loadCoupons(): List<Coupon> {
+        val dd = Firebase.firestore;
+
         return listOf<Coupon>(
             Coupon(R.string.coupon1, R.drawable.image1),
             Coupon(R.string.coupon2, R.drawable.image2),
