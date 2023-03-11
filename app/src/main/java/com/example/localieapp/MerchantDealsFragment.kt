fd0ca421a644c86d43afbd6d96550ab7f31872c5
Khoa Ly <kly27@calpoly.edu>
2023-03-11 15:59:41 -0800

    feat: merchant deals screen

--- app/src/main/java/com/example/localieapp/MerchantDealsFragment.kt
+++ app/src/main/java/com/example/localieapp/MerchantDealsFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.localieapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MerchantProfile.newInstance] factory method to
+ * Use the [MerchantDeals.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MerchantProfile : Fragment() {
+class MerchantDealsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +34,7 @@ class MerchantProfile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_merchant_profile, container, false)
+        return inflater.inflate(R.layout.fragment_merchant_deals, container, false)
     }
 
     companion object {
@@ -45,12 +44,12 @@ class MerchantProfile : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MerchantProfile.
+         * @return A new instance of fragment MerchantDeals.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MerchantProfile().apply {
+            MerchantDealsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/localieapp/MerchantProfileFragment.kt
+++ app/src/main/java/com/example/localieapp/MerchantProfileFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MerchantDeals.newInstance] factory method to
+ * Use the [MerchantProfile.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MerchantDeals : Fragment() {
+class MerchantProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class MerchantDeals : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_merchant_deals, container, false)
+        return inflater.inflate(R.layout.fragment_merchant_profile, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class MerchantDeals : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment MerchantDeals.
+         * @return A new instance of fragment MerchantProfile.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MerchantDeals().apply {
+            MerchantProfileFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
