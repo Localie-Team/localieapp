17ab99cd2872fc1c5742be681ead3f236b2427c3
kareemdarghous <kdarghous@gmail.com>
2023-04-27 14:12:50 -0700

    added checkable

--- app/src/main/java/com/example/localieapp/adapter/EarnGridAdapter.kt
+++ app/src/main/java/com/example/localieapp/adapter/EarnGridAdapter.kt
@@ -12,6 +12,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.localieapp.R
 import com.example.localieapp.model.Coupon
+import com.google.android.material.card.MaterialCardView
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
@@ -23,7 +24,7 @@ class EarnGridAdapter(private val context: Context, private val dataset: List<Co
     }.toMap()
 
     // Class holds references to the views in each item of the RecyclerView
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(val view: MaterialCardView) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title);
         val imageView: ImageView = view.findViewById(R.id.item_image);
     }
@@ -34,7 +35,7 @@ class EarnGridAdapter(private val context: Context, private val dataset: List<Co
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.adapter_grid_item, parent, false)
 
-        return ItemViewHolder(adapterLayout)
+        return ItemViewHolder(adapterLayout as MaterialCardView)
     }
 
     // This function returns the number of items in the list of coupons
@@ -67,5 +68,15 @@ class EarnGridAdapter(private val context: Context, private val dataset: List<Co
                     .into(holder.imageView)
             }
         }
+        holder.itemView.setOnClickListener(View.OnClickListener() {
+            Log.d("onBindViewHolder:", position.toString())
+        })
+
+        holder.itemView.setOnLongClickListener(View.OnLongClickListener()
+        {
+            holder.view.setChecked(!holder.view.isChecked)
+            true
+        }
+        )
     }
 }
